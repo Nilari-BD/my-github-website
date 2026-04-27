8f5c237dedb5160e71d0345ba94aba94ba9bbef2
Nilari Gabriela Berger Díaz <69632651+Nilari-BD@users.noreply.github.com>
2026-04-27 03:48:11 -0400

    Add accordion functionality for skills section

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ acc.forEach(button => {
     button.classList.toggle("active");
   });
 });
+
+const acc = document.querySelectorAll(".skills-accordion-btn");
+
+acc.forEach(button => {
+  button.addEventListener("click", () => {
+    const panel = button.nextElementSibling;
+    panel.style.display = (panel.style.display === "block") ? "none" : "block";
+    button.classList.toggle("active");
+  });
+});
